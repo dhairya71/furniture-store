bbea322669864aca61c2c4711f655a02b813199c
dhairya71 <dabi1998dhairya@gmail.com>
2020-11-17 20:52:37 +0530

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAz_EQIGom7rJ7qz3r2hdW68sFupDB_Hzs",
+  authDomain: "amit-exports.firebaseapp.com",
+  databaseURL: "https://amit-exports.firebaseio.com",
+  projectId: "amit-exports",
+  storageBucket: "amit-exports.appspot.com",
+  messagingSenderId: "143505476619",
+  appId: "1:143505476619:web:7900830c05b6544a350b0e",
+  measurementId: "G-8K9QJJKD2H",
+};
