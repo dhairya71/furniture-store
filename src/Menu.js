4449a26a6c28b4fc1c8ab5e9f1885f153ac2f7cc
dhairya71 <dabi1998dhairya@gmail.com>
2020-11-17 20:53:06 +0530

    updated

---
+++ src/Menu.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export const Menu = [
+  {
+    title: "Living room",
+
+    child: [
+      {
+        title: "sofa",
+        index: 11,
+      },
+    ],
+  },
+  {
+    title: "Drawing room",
+    child: [
+      {
+        title: "sofa",
+        index: 11,
+      },
+    ],
+  },
+  {
+    title: "Dining room",
+    child: [
+      {
+        title: "sofa",
+        index: 11,
+      },
+    ],
+  },
+  {
+    title: "Bedroom room",
+    child: [
+      {
+        title: "sofa",
+        index: 11,
+      },
+    ],
+  },
+];
---
+++ src/Products.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+export const Products = [
+  {
+    title: "Chair",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202035/0003/larnaca-teak-x-all-weather-weave-club-chair-c.jpg",
+  },
+  {
+    title: "Fan",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0030/calistoga-outdoor-sofa-c.jpg",
+  },
+  {
+    title: "Table",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0159/aerin-east-hampton-outdoor-club-chair-c.jpg",
+  },
+  {
+    title: "Couch",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0207/ojai-outdoor-club-chair-c.jpg",
+  },
+  {
+    title: "sofa",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0073/bridgehampton-outdoor-club-chair-c.jpg",
+  },
+  {
+    title: "sidetable",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0185/larnaca-outdoor-teak-club-chair-c.jpg",
+  },
+  {
+    title: "sidetable",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0182/larnaca-outdoor-teak-chaise-c.jpg",
+  },
+  {
+    title: "sidetable",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0052/manchester-outdoor-sofa-82-c.jpg",
+  },
+  {
+    title: "Chair",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202035/0003/larnaca-teak-x-all-weather-weave-club-chair-c.jpg",
+  },
+  {
+    title: "Fan",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0030/calistoga-outdoor-sofa-c.jpg",
+  },
+  {
+    title: "Table",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0159/aerin-east-hampton-outdoor-club-chair-c.jpg",
+  },
+  {
+    title: "Couch",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0207/ojai-outdoor-club-chair-c.jpg",
+  },
+  {
+    title: "sofa",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0073/bridgehampton-outdoor-club-chair-c.jpg",
+  },
+  {
+    title: "sidetable",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0185/larnaca-outdoor-teak-club-chair-c.jpg",
+  },
+  {
+    title: "sidetable",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0182/larnaca-outdoor-teak-chaise-c.jpg",
+  },
+  {
+    title: "sidetable",
+    price: "200",
+    image:
+      "https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202038/0052/manchester-outdoor-sofa-82-c.jpg",
+  },
+];
